6e404dc0e009c97df8cd4f5c8fb273397d7cd5b9
Nick Calibey <nick.calibey@gmail.com>
2019-03-17 17:02:34 -0500

    Implement 'add' command

--- notes.js
+++ notes.js
@@ -1,5 +1,45 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
 const getNotes = () => {
-  return "Your notes..."
+  return "Your notes...";
 };
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body,
+    });
+
+    saveNotes(notes);
+    console.log(chalk.green('New note added!'));
+  } else {
+    console.log(chalk.red('Note title already taken!'));
+  }
+};
+
+const saveNotes = (notes) => {
+  const json = JSON.stringify(notes);
+
+  fs.writeFileSync('notes.json', json);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes,
+  addNote,
+};
