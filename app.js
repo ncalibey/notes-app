d2bc333224521178d1c86321bbbebffed541ada8
Nick Calibey <nick.calibey@gmail.com>
2019-03-16 20:07:16 -0500

    Add skeletal add, remove, list, and read commands

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-console.log(chalk.green.bold.inverse('Success!'));
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler () {
+    console.log('Adding a new note!');
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler () {
+    console.log('Removing the note');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler () {
+    console.log('Listing your notes!');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler () {
+    console.log("Here's the note!");
+  },
+});
+
+console.log(yargs.argv);
