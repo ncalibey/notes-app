de2d57d32e657ef6627e36e25fd424f113758049
Nick Calibey <nick.calibey@gmail.com>
2019-03-16 19:42:01 -0500

    Add 'chalk'

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
-console.log(getNotes());
-
-console.log(validator.isURL('htts://mead.io'));
+console.log(chalk.green.bold.inverse('Success!'));
