5a200c28a87d16371aa91895855a57303801702a
Nick Calibey <nick.calibey@gmail.com>
2019-03-17 17:27:21 -0500

    Add 'remove' and 'list' commands

--- app.js
+++ app.js
@@ -30,8 +30,15 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
-  handler () {
-    console.log('Removing the note');
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler (argv) {
+    notes.removeNote(argv.title);
   },
 });
 
@@ -40,7 +47,7 @@ yargs.command({
   command: 'list',
   describe: 'List your notes',
   handler () {
-    console.log('Listing your notes!');
+    notes.listNotes();
   },
 });
 
--- notes.js
+++ notes.js
@@ -22,11 +22,35 @@ const addNote = (title, body) => {
   }
 };
 
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(note => note.title !== title);
+
+  if (notes.length !== notesToKeep.length) {
+    saveNotes(notesToKeep);
+    console.log(chalk.green.inverse('SUCCESS:') + chalk.green(` note "${title}" was deleted!`));
+  } else {
+    console.log(chalk.red.inverse('ERROR:') + chalk.red(' Note does not exist!'));
+  }
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.underline.bold('Your notes:'));
+
+  for (let i = 0; i < notes.length; i +=1) {
+    console.log(chalk.green(notes[i].title));
+  }
+};
+
+// Helper functions
 const saveNotes = (notes) => {
   const json = JSON.stringify(notes);
 
   fs.writeFileSync('notes.json', json);
 };
+
+
 
 const loadNotes = () => {
   try {
@@ -42,4 +66,6 @@ const loadNotes = () => {
 module.exports = {
   getNotes,
   addNote,
+  removeNote,
+  listNotes,
 };
