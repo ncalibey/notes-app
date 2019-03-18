ee7512eb19efdf77c1c41d0562162a1b9f39c9f0
Nick Calibey <nick.calibey@gmail.com>
2019-03-17 19:44:15 -0500

    Revert version number

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const yargs = require('yargs');
 const notes = require('./notes');
 
 // Customize yargs version
-yargs.version('1.1.0');
+yargs.version('1.0.0');
 
 // Create add command
 yargs.command({
