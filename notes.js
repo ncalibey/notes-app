10d9b141aad37f0e12f36e836ba6e8eda6f569c5
Nick Calibey <nick.calibey@gmail.com>
2019-03-17 19:19:46 -0500

    Add list and read commands

--- notes.js
+++ notes.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => {
-  return "Your notes...";
+const messages = {
+  success (string) {
+    return chalk.green.inverse('SUCCESS:') + chalk.green(string);
+  },
+  error (string) {
+    return chalk.red.inverse('ERROR:') + chalk.red(string);
+  },
+  header (string) {
+    return chalk.underline.bold(string);
+  }
 };
 
 const addNote = (title, body) => {
@@ -16,9 +24,9 @@ const addNote = (title, body) => {
     });
 
     saveNotes(notes);
-    console.log(chalk.green('New note added!'));
+    console.log(messages.success(' New note added!'));
   } else {
-    console.log(chalk.red('Note title already taken!'));
+    console.log(messages.error(' Note title already taken!'));
   }
 };
 
@@ -28,18 +36,31 @@ const removeNote = (title) => {
 
   if (notes.length !== notesToKeep.length) {
     saveNotes(notesToKeep);
-    console.log(chalk.green.inverse('SUCCESS:') + chalk.green(` note "${title}" was deleted!`));
+    console.log(messages.success(` note "${title}" was deleted!`));
   } else {
-    console.log(chalk.red.inverse('ERROR:') + chalk.red(' Note does not exist!'));
+    console.log(messages.error(' Note does not exist!'));
   }
 };
 
 const listNotes = () => {
   const notes = loadNotes();
-  console.log(chalk.underline.bold('Your notes:'));
+
+  console.log(messages.header('Your notes:'));
 
   for (let i = 0; i < notes.length; i +=1) {
     console.log(chalk.green(notes[i].title));
+  }
+};
+
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find(note => note.title === title);
+
+  if (note) {
+    console.log(messages.header(note.title));
+    console.log(note.body);
+  } else {
+    console.log(messages.error(' Note does not exist!'));
   }
 };
 
@@ -62,8 +83,8 @@ const loadNotes = () => {
 };
 
 module.exports = {
-  getNotes,
   addNote,
   removeNote,
   listNotes,
+  readNote,
 };
